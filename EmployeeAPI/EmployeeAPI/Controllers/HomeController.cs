6a45d4c34645716a743df2fc7b2fdd5fd5461fe2
OmkarMohite2001 <omkarmohite85@gmail.com>
2026-08-03 20:33:45 +0530

    Session 01 And Session 02

---
+++ EmployeeAPI/EmployeeAPI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
